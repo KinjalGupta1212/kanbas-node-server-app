e242c3109ae93bef614589f1cd8543b4fa32f1cc
Kinjal Gupta <kinjalgupta1212@gmail.com>
2024-04-02 15:27:26 -0400

    a5 minor changes

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
-
 const app = express()
 app.use(cors());
 app.use(express.json());
--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -10,6 +10,7 @@ export default function CourseRoutes(app) {
     }
     res.send(course);
   });
+
   app.put("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     const course = req.body;
@@ -18,18 +19,21 @@ export default function CourseRoutes(app) {
     );
     res.sendStatus(204);
   });
+
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     Database.courses = Database.courses
       .filter((c) => c._id !== id);
     res.sendStatus(204);
   });
+
   app.post("/api/courses", (req, res) => {
     const course = { ...req.body,
       _id: new Date().getTime().toString() };
     Database.courses.push(course);
     res.send(course);
   });
+  
   app.get("/api/courses", (req, res) => {
     const courses = Database.courses;
     res.send(courses);
